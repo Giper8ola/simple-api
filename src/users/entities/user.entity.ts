4397bc3f761c14a99aca95a73a873d627e9e0cad
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-04 19:00:45 +0300

    feat(entity): add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
+
+@Table
+export class User extends Model<User> {
+    @Column({
+        type: DataType.STRING,
+        unique: true,
+        allowNull: false
+    })
+    name: string;
+
+    @Column({
+        type: DataType.STRING,
+        unique: true,
+        allowNull: false
+    })
+    email: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    password: string;
+}
