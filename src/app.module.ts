d586ecdf8574815ce653e3701f05487d224ee4ec
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-04 19:38:40 +0300

    feat(module): add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
@@ -11,7 +12,8 @@ import { UsersModule } from './users/users.module';
         ConfigModule.forRoot({
             isGlobal: true
         }),
-        UsersModule
+        UsersModule,
+        AuthModule
     ],
     controllers: [AppController],
     providers: [AppService]
