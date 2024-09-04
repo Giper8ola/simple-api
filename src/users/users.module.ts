5bc1c4df040528526287ab5bbd9528e8d0b9f4fb
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-04 19:17:14 +0300

    feat(repos): implement user repository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
+import { USER_REPOSITORY } from '../core/constants';
 
 @Module({
     controllers: [UsersController],
-    providers: [UsersService]
+    providers: [
+        UsersService,
+        {
+            provide: USER_REPOSITORY,
+            useValue: User
+        }
+    ]
 })
 export class UsersModule {}
