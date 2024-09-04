64bd383509788f5e0c79b81029798cf52956e5b4
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-04 19:04:43 +0300

    feat(dto): add user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsEmail, IsString } from 'class-validator';
+
+export class UserDto {
+    @IsString()
+    name: string;
+
+    @IsString()
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    password: string;
+}
