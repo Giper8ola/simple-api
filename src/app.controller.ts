c79890fa642b6582f7feb691249d7f3a35847768
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-05 01:12:38 +0300

    feat(module): add signUp method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from '@nestjs/passport';
+import { UsersService } from './users/users.service';
+import { CreateUserDto } from './users/dto/create-user.dto';
 
 @Controller()
 export class AppController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(
+        private readonly authService: AuthService,
+        private readonly userService: UsersService
+    ) {}
 
     @UseGuards(AuthGuard('local'))
     @Post('auth/login')
     async login(@Request() req) {
         return await this.authService.login(req.user);
+    }
+
+    @Post('auth/signup')
+    async signUp(@Body() createUserDto: CreateUserDto) {
+        return await this.userService.create(createUserDto);
     }
 }
