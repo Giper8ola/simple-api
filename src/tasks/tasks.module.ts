76c8f38c7df0ba1da1ddc86ebd9118c29ecf075b
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-05 15:35:45 +0300

    feat(provider): implement task repo to task module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { TASK_REPOSITORY } from '../core/constants';
+import { Task } from './entities/task.entity';
 
 @Module({
     controllers: [TasksController],
-    providers: [TasksService]
+    providers: [
+        TasksService,
+        {
+            provide: TASK_REPOSITORY,
+            useValue: Task
+        }
+    ]
 })
 export class TasksModule {}
