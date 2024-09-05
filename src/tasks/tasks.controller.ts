77f89e256d975c96a009e24c703328825bedcebe
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-05 17:47:13 +0300

    feat: add auth for user's and task's endpoints

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,36 +1,40 @@
 import {
-    Controller,
-    Get,
-    Post,
     Body,
-    Patch,
-    Param,
+    Controller,
     Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
     Query
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { QueryFindAllDto } from './dto/query-params.dto';
+import { AuthWithRole } from '../core/decorators/AuthWithRole.decorator';
+import { RolesEnum } from '../core/enums/role';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private readonly tasksService: TasksService) {}
 
+    @AuthWithRole(RolesEnum.USER)
     @Post('create')
     create(@Body() createTaskDto: CreateTaskDto) {
         return this.tasksService.create(createTaskDto);
     }
-
+    @AuthWithRole(RolesEnum.USER)
     @Get('find/all')
     findAll(@Query() queryParams: QueryFindAllDto) {
         return this.tasksService.findAll(queryParams);
     }
+    @AuthWithRole(RolesEnum.USER)
     @Patch(':id')
     update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
         return this.tasksService.update(+id, updateTaskDto);
     }
-
+    @AuthWithRole(RolesEnum.USER)
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.tasksService.remove(+id);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,26 +12,29 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { QueryFindAllDto } from './dto/query-params.dto';
+import { AuthWithRole } from '../core/decorators/AuthWithRole.decorator';
+import { RolesEnum } from '../core/enums/role';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
+    @AuthWithRole(RolesEnum.ADMIN)
     @Post('create')
     async create(@Body() createUserDto: CreateUserDto) {
         return await this.usersService.create(createUserDto);
     }
-
+    @AuthWithRole(RolesEnum.ADMIN)
     @Get('find/all')
     async findAll(@Query() queryParams: QueryFindAllDto) {
         return await this.usersService.findAll(queryParams);
     }
-
+    @AuthWithRole(RolesEnum.ADMIN)
     @Get('find/one')
     async findOne(@Query('name') name: string) {
         return await this.usersService.findOne(name);
     }
-
+    @AuthWithRole(RolesEnum.USER)
     @Patch(':id')
     async update(
         @Param('id') id: string,
@@ -40,6 +43,7 @@ export class UsersController {
         return await this.usersService.update(+id, updateUserDto);
     }
 
+    @AuthWithRole(RolesEnum.ADMIN)
     @Delete(':id')
     async remove(@Param('id') id: string) {
         return await this.usersService.remove(+id);
