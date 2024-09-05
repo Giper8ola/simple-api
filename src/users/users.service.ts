9c8ef276d1a1f35d3e5fb1345cf1d5dcdfb737a9
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-05 16:57:32 +0300

    feat(service): add logic to findAll for UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,18 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { USER_REPOSITORY } from '../core/constants';
+import { TASK_REPOSITORY, USER_REPOSITORY } from '../core/constants';
 import { User } from './entities/user.entity';
 import { RolesEnum } from '../core/enums/role';
 import { QueryFindAllDto } from './dto/query-params.dto';
+import { Task } from '../tasks/entities/task.entity';
+import { Op } from 'sequelize';
 
 @Injectable()
 export class UsersService {
     constructor(
-        @Inject(USER_REPOSITORY) private readonly userRepository: typeof User
+        @Inject(USER_REPOSITORY) private readonly userRepository: typeof User,
+        @Inject(TASK_REPOSITORY) private readonly taskRepository: typeof Task
     ) {}
 
     async create(createUserDto: CreateUserDto) {
@@ -20,7 +23,77 @@ export class UsersService {
     }
 
     async findAll(query: QueryFindAllDto) {
-        return await this.userRepository.findAll();
+        const {
+            page = 1,
+            limit = 20,
+            sortBy = 'createdAt',
+            taskFilter = ''
+        } = query;
+
+        const offset = (page - 1) * limit;
+
+        let whereObject = {};
+        const regex = /^(>=|<=|>|<|==)\d+$/;
+
+        if (taskFilter && regex.test(taskFilter)) {
+            const [symbol] = taskFilter.match(/\d+/g);
+            const [number] = taskFilter.match(/>=|<=|>|<|==/g);
+
+            switch (symbol) {
+                case '>=':
+                    {
+                        whereObject = {
+                            taskAmount: {
+                                [Op.gte]: Number(number)
+                            }
+                        };
+                    }
+                    break;
+                case '<=':
+                    {
+                        whereObject = {
+                            taskAmount: {
+                                [Op.lte]: Number(number)
+                            }
+                        };
+                    }
+                    break;
+                case '>':
+                    {
+                        whereObject = {
+                            taskAmount: {
+                                [Op.gt]: Number(number)
+                            }
+                        };
+                    }
+                    break;
+                case '<':
+                    {
+                        whereObject = {
+                            taskAmount: {
+                                [Op.lt]: Number(number)
+                            }
+                        };
+                    }
+                    break;
+                case '==':
+                    {
+                        whereObject = {
+                            taskAmount: {
+                                [Op.eq]: Number(number)
+                            }
+                        };
+                    }
+                    break;
+            }
+        }
+        return await this.userRepository.findAndCountAll({
+            where: whereObject,
+            attributes: ['id', 'name', 'email'],
+            limit: limit,
+            offset: offset,
+            order: [[sortBy, 'DESC']]
+        });
     }
 
     async findOne(username: string) {
