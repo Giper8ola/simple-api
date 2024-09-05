696056e954c788c74989607ad8eafc157f5a5abd
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-05 17:06:48 +0300

    feat(dto): add QueryFindAllDto for findAll task method

---
+++ src/tasks/dto/query-params.dto.ts
@@ -0,0 +1,23 @@
+import { IsNumber, IsOptional, IsString, Max, Min } from 'class-validator';
+
+export class QueryFindAllDto {
+    @IsOptional()
+    @IsNumber()
+    @Min(1)
+    @Max(100)
+    page?: number = 1;
+
+    @IsOptional()
+    @IsNumber()
+    @Min(1)
+    @Max(100)
+    limit?: number = 10;
+
+    @IsOptional()
+    @IsString()
+    sortBy?: string = 'createdAt';
+
+    @IsOptional()
+    @IsString()
+    titleLength?: string;
+}
