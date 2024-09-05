d09351f3a5cc0288e8c90c70c05e43badc18605c
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-05 15:23:50 +0300

    feat(model): create and implement task model

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,22 @@
-export class Task {}
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+@Table
+export class Task extends Model<Task> {
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    title: string;
+
+    @Column({
+        type: DataType.STRING,
+        defaultValue: ''
+    })
+    description: string;
+
+    @Column({
+        type: DataType.BOOLEAN,
+        defaultValue: false
+    })
+    completed: boolean;
+}
