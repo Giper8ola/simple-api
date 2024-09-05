4b1680baf23ea2c539fc827faf8b77eb012ea325
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-05 17:26:21 +0300

    feat(service): add logic to findAll method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,6 +3,8 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { TASK_REPOSITORY } from '../core/constants';
 import { Task } from './entities/task.entity';
+import { QueryFindAllDto } from './dto/query-params.dto';
+import { col, fn, Op } from 'sequelize';
 
 @Injectable()
 export class TasksService {
@@ -14,8 +16,47 @@ export class TasksService {
         return await this.taskRepository.create(createTaskDto);
     }
 
-    async findAll() {
-        return await this.taskRepository.findAll();
+    async findAll(query: QueryFindAllDto) {
+        const { page, limit, sortBy, titleLength } = query;
+
+        const offset = (page - 1) * limit;
+
+        const whereObject = {
+            [Op.and]: [fn('CHAR_LENGTH', col('title')), {}]
+        };
+
+        const regex = /^(>=|<=|>|<|==)\d+$/;
+
+        if (titleLength && regex.test(titleLength)) {
+            const [symbol] = titleLength.match(/\d+/g);
+            const [number] = titleLength.match(/>=|<=|>|<|==/g);
+
+            switch (symbol) {
+                case '>=':
+                    whereObject[Op.and][1] = { [Op.gte]: Number(number) };
+                    break;
+                case '<=':
+                    whereObject[Op.and][1] = { [Op.lte]: Number(number) };
+                    break;
+                case '>':
+                    whereObject[Op.and][1] = { [Op.gt]: Number(number) };
+                    break;
+                case '<':
+                    whereObject[Op.and][1] = { [Op.lt]: Number(number) };
+                    break;
+                case '==':
+                    whereObject[Op.and][1] = { [Op.eq]: Number(number) };
+                    break;
+            }
+        }
+
+        return await this.taskRepository.findAndCountAll({
+            where: whereObject,
+            attributes: ['title', 'description', 'userId'],
+            limit: limit,
+            offset: offset,
+            order: [[sortBy, 'DESC']]
+        });
     }
 
     async findAllByUserId(id: number) {
