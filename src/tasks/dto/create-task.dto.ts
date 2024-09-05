218150bbfeb73274654b1c4c54cb0325d2f8a355
Giper8ola <matwey.peregudow2100@gmail.com>
2024-09-05 15:24:39 +0300

    feat(dto): add task's dto

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,12 @@
-export class CreateTaskDto {}
+import { IsBoolean, IsString } from 'class-validator';
+
+export class CreateTaskDto {
+    @IsString()
+    title: string;
+
+    @IsString()
+    description: string;
+
+    @IsBoolean()
+    completed: boolean;
+}
